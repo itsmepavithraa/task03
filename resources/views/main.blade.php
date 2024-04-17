97ca890872a8d28c6b2eb282a8aef370f9d1d8bd
pavithraa20102004 <pavithraa20102004@gmail.com>
2024-04-17 11:34:05 +0530

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Task 3</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body class="bg-light">
+
+  <p class="fs-6 fw-medium text-center font-monospace">News <span class="text-primary">Room</span></p>
+  <p class="fw-bold text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br> Quisquam eum nulla molestias voluptates qui dolorum.</p>
+
+  <!-- 1 -->
+  <div class="container">
+    <div class="row mb-4"> 
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+      
+      <!-- 2 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/123335/pexels-photo-123335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+      
+      <!-- 3 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/7974/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mb-4"> 
+      <!-- 4 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/1779487/pexels-photo-1779487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+      
+      <!-- 5 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/129208/pexels-photo-129208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+      
+     <!-- 6 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/633409/pexels-photo-633409.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <!-- 7 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img src="https://images.pexels.com/photos/1261820/pexels-photo-1261820.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" class="card-img-top" style="margin-bottom: 0px; margin: 10px; width: 50%;">
+          <div class="card-body" style="padding-top: 0px;">
+            <h6 class="card-title text-primary mb-1 fw-bold">Technology</h6>
+            <p class="fs-6 card-text mb-2 fw-bold">some sample text here.</p>
+            <a href="#" class="btn btn-primary mt-1">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
